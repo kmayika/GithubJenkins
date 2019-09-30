844399c9da301114059a7d678a1e48e06304ec53
kwezi <kwezi>
2019-09-30 11:43:02 +0200

    first commit

---
+++ GithubJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Jenkins and Github!");
+        }
+    }
+}
